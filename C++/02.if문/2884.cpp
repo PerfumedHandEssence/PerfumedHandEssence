9c8c30e30ee0bfd463097be3f2646da5c792255f
PerfumedHandEssence <67088292+PerfumedHandEssence@users.noreply.github.com>
2021-09-28 20:52:37 +0900

    Create 2884.cpp

---
+++ C++/02.if문/2884.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (b < 45)
+        if (a == 0)
+            cout << 23 << " " << b + 15;
+        else
+            cout << a - 1 << " " << b + 15;
+    else
+        cout << a << " " << b - 45;
+}
